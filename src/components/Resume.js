42d34b88e70b3f6834d857c6f7b29bdc8f205c0f
Jim Tyhurst <jim@tyhurst.com>
2018-09-23 01:46:29 -0700

    Added a 'Clients' section to the 'Resume'.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-import Education from "./Education";
 import Work from "./Work";
+import Clients from "./Clients";
+import Education from "./Education";
 
 const Resume = () => (
   <section id="resume">
     <Work />
+    <Clients />
     <Education />
   </section>
 );

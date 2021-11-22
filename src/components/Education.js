9bdc8c498ce4f0da423bb6f9f3f2f38d0780c931
Jim Tyhurst <jim@tyhurst.com>
2021-11-22 03:08:26 -0800

    Target links to course descriptions.

--- src/components/Education.js
+++ src/components/Education.js
@@ -108,7 +108,15 @@ const Education = () => (
 
       <div className="row item">
         <div className="twelve columns">
-          <h3>University of California, Los Angeles (UCLA)</h3>
+          <h3>University of California, Los Angeles (
+            <a
+              href="http://linguistics.ucla.edu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              UCLA
+            </a>
+            )</h3>
           <p className="info">
             M.A.{' '}
             <a

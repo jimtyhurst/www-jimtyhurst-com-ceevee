3c9ec8bc9d562fc79d188a0b35d3f4a2a29a4eab
Jim Tyhurst <jim@tyhurst.com>
2021-11-21 22:08:01 -0800

    Revised resume. Upgraded Node.js.

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import resumePdf from "../assets/resources/JimTyhurst_Resume.pdf";
-
 const Clients = () => (
   <div className="row clients">
     <div className="three columns header-col">
@@ -16,8 +14,6 @@ const Clients = () => (
           <div className="info">
             <p>
               See my{" "}
-              <a href={resumePdf} target="_blank" rel="noopener noreferrer">PDF resume</a>
-              {" "}or my{" "}
               <a href="https://www.linkedin.com/in/jimtyhurst" target="_blank" rel="noopener noreferrer">LinkedIn profile</a>
               {" "}for a listing of my consulting clients and projects.
             </p>
